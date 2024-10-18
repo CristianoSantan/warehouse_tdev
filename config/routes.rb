3fe17f499b38e28a68421666cc0d4fc6e2658fd7
CristianoSantan <cristiano.mer@gmail.com>
2024-10-18 11:02:44 -0300

    23. Associações: belongs_to

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
-
+  resources :product_models, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,18 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_160331) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_18_133745) do
+  create_table "product_models", force: :cascade do |t|
+    t.string "name"
+    t.integer "weight"
+    t.integer "width"
+    t.integer "height"
+    t.integer "depth"
+    t.string "sku"
+    t.integer "supplier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supplier_id"], name: "index_product_models_on_supplier_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "corporate_name"
     t.string "brand_name"
@@ -34,4 +36,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_160331) do
     t.string "cep"
     t.string "description"
   end
+
+  add_foreign_key "product_models", "suppliers"
 end
