91fa31ce135e65691886a89e88d271bad13f66d2
CristianoSantan <cristiano.mer@gmail.com>
2024-10-21 09:21:20 -0300

    29. Criando um pedido

---
+++ spec/system/orders/user_register_order_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Usuário cadastra um pedido" do
+  it "e deve estar autenticado" do
+    visit root_path
+    click_on 'Registrar Pedido'
+
+    expect(current_path).to eq new_user_session_path
+  end
+  
+  it "com sucesso" do
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+    Warehouse.create!(name: 'Belo Horizonte', code: 'BHZ', area: 20000, cep: '64000-000', city: 'Belo Horizonte', 
+      description: 'Galpão mineiro', address: 'Av Tiradentes, 20')
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+      address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado para cargas internacionais')
+    Supplier.create!(corporate_name: 'LG Corporation', brand_name: 'LG', city: 'Manaus', 
+      registration_number: '100', full_address: "Rua Amazonia, 180", state: "AM", email: "contato@lg.com.br")
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
+    ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10, sku: 'TV32-SAMSU-XPT090', supplier: supplier)
+
+    login_as(user)
+    visit root_path
+    click_on 'Registrar Pedido'
+    select warehouse.name, from: 'Galpão Destino'
+    select supplier.corporate_name, from: 'Fornecedor'
+    fill_in "Data Prevista de Entrega",	with: "20/12/2022" 
+    click_on 'Gravar'
+
+    expect(page).to have_content 'Pedido registrado com sucesso.'
+    expect(page).to have_content 'Galpão Destino: Aeroporto SP'
+    expect(page).to have_content 'Fornecedor: Samsung Eletronicos LTDA'
+    expect(page).to have_content 'Usuário Responsável: João | <joao@email.com>'
+    expect(page).to have_content 'Data Prevista de Entrega: 20/12/2022'
+    expect(page).not_to have_content 'Galpão Manaus'
+    expect(page).not_to have_content 'LG Corporation'
+  end
+end
