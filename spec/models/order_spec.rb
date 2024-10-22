26cd773b108a9b9f3b323eedab95fc792b0e5ca3
CristianoSantan <cristiano.mer@gmail.com>
2024-10-22 09:25:59 -0300

    32. Validações personalizadas

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,11 +9,46 @@ RSpec.describe Order, type: :model do
       supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', 
         city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
       order = Order.new(user: user, warehouse: warehouse, supplier: supplier, 
-        estimated_delivery_date: '2022-10-01')
+        estimated_delivery_date:  1.day.from_now)
 
       result = order.valid?
 
       expect(result).to be true 
+    end
+
+    it "data estimada de entrega deve ser obrigatória" do
+      order = Order.new(estimated_delivery_date: '')
+
+      order.valid?
+      result = order.errors.include?(:estimated_delivery_date)
+
+      expect(result).to be true 
+    end
+
+    it "data estimada de entrega não deve ser passada" do
+      order = Order.new(estimated_delivery_date: 1.day.ago)
+
+      order.valid?
+
+      expect(order.errors.include?(:estimated_delivery_date)).to be true
+      expect(order.errors[:estimated_delivery_date]).to include("deve ser futura.")
+    end
+    
+    it "data estimada de entrega não deve ser igual a hoje" do
+      order = Order.new(estimated_delivery_date: Date.today)
+
+      order.valid?
+
+      expect(order.errors.include?(:estimated_delivery_date)).to be true
+      expect(order.errors[:estimated_delivery_date]).to include("deve ser futura.")
+    end
+
+    it "data estimada de entrega deve ser igual ou maior do que amanhã" do
+      order = Order.new(estimated_delivery_date: 1.day.from_now)
+
+      order.valid?
+
+      expect(order.errors.include?(:estimated_delivery_date)).to be false
     end
   end
   
@@ -25,7 +60,7 @@ RSpec.describe Order, type: :model do
       supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', 
         city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
       order = Order.new(user: user, warehouse: warehouse, supplier: supplier, 
-        estimated_delivery_date: '2022-10-01')
+        estimated_delivery_date: 1.day.from_now)
       
       order.save!
       result = order.code
@@ -41,9 +76,9 @@ RSpec.describe Order, type: :model do
       supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', 
         city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
       first_order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, 
-        estimated_delivery_date: '2022-10-01')
+        estimated_delivery_date: 1.day.from_now)
       second_order = Order.new(user: user, warehouse: warehouse, supplier: supplier, 
-        estimated_delivery_date: '2022-11-15')
+        estimated_delivery_date: 1.day.from_now)
       
       second_order.save!
 
