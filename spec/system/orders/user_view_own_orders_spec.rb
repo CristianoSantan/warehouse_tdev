c335177b292aaaf6bcc104a5c204d12168c0f8a4
CristianoSantan <cristiano.mer@gmail.com>
2024-10-29 10:02:22 -0300

    38. Itens para um pedido

--- spec/system/orders/user_view_own_orders_spec.rb
+++ spec/system/orders/user_view_own_orders_spec.rb
@@ -64,6 +64,28 @@ describe "Usuário vê seus próprios pedidos" do
     expect(current_path).not_to eq order_path(first_order.id)
     expect(current_path).to eq root_path
     expect(page).to have_content 'Você não possui acesso a este pedido.' 
+  end
+  it "e vê itens do pedido" do
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
+    product_a = ProductModel.create!(name: 'Produto A', weight: 1, width: 10, height: 20, depth:30, supplier: supplier, sku: 'Product-A')
+    product_b = ProductModel.create!(name: 'Produto B', weight: 1, width: 10, height: 20, depth:30, supplier: supplier, sku: 'Product-B')
+    product_c = ProductModel.create!(name: 'Produto C', weight: 1, width: 10, height: 20, depth:30, supplier: supplier, sku: 'Product-C')
+    andre = User.create!(name: 'Andre', email: 'andre@email.com', password: 'password')
+    joao = User.create!(name: 'Joao', email: 'joao@email.com', password: 'password')
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+      address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado para cargas internacionais')
+    order = Order.create!(user:joao, warehouse:warehouse, supplier:supplier, estimated_delivery_date: 1.day.from_now)
 
+    OrderItem.create!(product_model: product_a, order: order, quantity: 19)
+    OrderItem.create!(product_model: product_b, order: order, quantity: 12)
+
+    login_as joao
+    visit root_path
+    click_on "Meus Pedidos"
+    click_on order.code
+
+    expect(page).to have_content 'Itens do Pedido'
+    expect(page).to have_content '19 x Produto A'
+    expect(page).to have_content '12 x Produto B'
   end
 end
