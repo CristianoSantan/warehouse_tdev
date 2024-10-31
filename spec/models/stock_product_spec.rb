5bc704d2c5172539369b2c6ca29cc2422fac54c0
CristianoSantan <cristiano.mer@gmail.com>
2024-10-31 11:44:01 -0300

    43. Saída de itens do estoque.

--- spec/models/stock_product_spec.rb
+++ spec/models/stock_product_spec.rb
@@ -35,4 +35,37 @@ RSpec.describe StockProduct, type: :model do
       expect(stock_product.serial_number).to eq original_serial_number
     end
   end
+
+  describe "#available?" do
+    it "true se não tiver destino" do
+      user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+      warehouse = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
+        address: 'Av do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
+      supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', 
+        city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, 
+        estimated_delivery_date:  1.day.from_now)
+      product = ProductModel.create!(name: 'Cadeira Gamer', weight: 5, height: 70, width: 80, sku: 'CGMER-XPTO-888', supplier: supplier)
+
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+
+      expect(stock_product.available?).to eq true
+    end
+    
+    it "false se não tiver destino" do
+      user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+      warehouse = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
+        address: 'Av do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
+      supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', 
+        city: 'São Paulo', registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
+      order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, 
+        estimated_delivery_date:  1.day.from_now)
+      product = ProductModel.create!(name: 'Cadeira Gamer', weight: 5, height: 70, width: 80, sku: 'CGMER-XPTO-888', supplier: supplier)
+
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+      stock_product.create_stock_product_destination!(recipient: 'João', address: "Rua do Joao")
+
+      expect(stock_product.available?).to eq false
+    end
+  end
 end
--- spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe "Usuário vê o estoque" do
   it "na tela do galpão" do
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
     warehouse = Warehouse.new(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
             address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
             description: 'Galpão destinado para cargas internacionais')
-    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
     supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', city: 'São Paulo', 
       registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
     order = Order.create!(user: user, supplier: supplier, warehouse: warehouse, estimated_delivery_date: 1.day.from_now)
@@ -23,9 +23,36 @@ describe "Usuário vê o estoque" do
     visit root_path
     click_on 'Aeroporto SP'
 
-    expect(page).to have_content 'Itens em Estoque'
-    expect(page).to have_content '3 x TV32-SAMSU-XPT090'
-    expect(page).to have_content '2 x NOTEI5-SAMSU-TLI99'
-    expect(page).not_to have_content 'SOU71-SAMSU-NOIZ77'
+    within("section#stock_products") do
+      expect(page).to have_content 'Itens em Estoque'
+      expect(page).to have_content '3 x TV32-SAMSU-XPT090'
+      expect(page).to have_content '2 x NOTEI5-SAMSU-TLI99'
+      expect(page).not_to have_content 'SOU71-SAMSU-NOIZ77'
+    end
+  end
+
+  it "e dá baixa em um item" do
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+    warehouse = Warehouse.new(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+            address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+            description: 'Galpão destinado para cargas internacionais')
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletronicos LTDA', brand_name: 'Samsung', city: 'São Paulo', 
+      registration_number: '100', full_address: "Av Nações Unidas, 1000", state: "SP", email: "contato@samsung.com.br")
+    order = Order.create!(user: user, supplier: supplier, warehouse: warehouse, estimated_delivery_date: 1.day.from_now)
+    product_tv = ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10, 
+      sku: 'TV32-SAMSU-XPT090', supplier: supplier)
+    2.times { StockProduct.create!(order: order, warehouse: warehouse, product_model: product_tv)}
+
+    login_as user
+    visit root_path
+    click_on 'Aeroporto SP'
+    select 'TV32-SAMSU-XPT090', from: 'Item para Saída'
+    fill_in "Destinatário",	with: "Maria Ferreira" 
+    fill_in "Endereço Destino",	with: "Rua das Palmeiras, 100 - Campinas - São Paulo" 
+    click_on 'Confirmar Retirada'
+
+    expect(current_path).to eq warehouse_path(warehouse.id)
+    expect(page).to have_content 'Item retirado com sucesso'
+    expect(page).to have_content '1 x TV32-SAMSU-XPT090'
   end
 end
