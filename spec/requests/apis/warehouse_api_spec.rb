341bdba54c4953c5c725ab942eb56c2b0b7f7e87
CristianoSantan <cristiano.mer@gmail.com>
2024-11-15 10:03:54 -0300

    46. APIs: criando galpões e tratando erros

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -52,5 +52,75 @@ describe "Warehouse API" do
       json_response =JSON.parse(response.body)
       expect(json_response).to eq []
     end
+
+    it "and raise internal error" do
+      allow(Warehouse).to receive(:all).and_raise(ActiveRecord::QueryCanceled)
+
+      get '/api/v1/warehouses'
+
+      expect(response).to have_http_status 500
+    end
+    
+  end
+
+  context "POST /api/v1/warehouses" do
+    it "success" do
+      warehouse_params = { 
+        warehouse: {
+          name: 'Belo Horizonte', 
+          code: 'BHZ', 
+          area: 20000, 
+          cep: '64000-000', 
+          city: 'Belo Horizonte', 
+          description: 'Galpão mineiro', 
+          address: 'Av Tiradentes, 20'
+          }
+        }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status :created
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response["name"]).to eq('Belo Horizonte')
+      expect(json_response["code"]).to eq('BHZ')
+      expect(json_response["area"]).to eq(20000)
+      expect(json_response["cep"]).to eq('64000-000')
+      expect(json_response["city"]).to eq('Belo Horizonte')
+      expect(json_response["description"]).to eq('Galpão mineiro')
+      expect(json_response["address"]).to eq('Av Tiradentes, 20')
+    end
+
+    it "fail if paramenters are not complete" do
+      warehouse_params = { warehouse: { name: "Aeroporto Curitiba", code: "CWB" } }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status 412
+      expect(response.body).not_to include "Nome não pode ficar em branco"
+      expect(response.body).not_to include "Código não pode ficar em branco"
+      expect(response.body).to include "Cidade não pode ficar em branco"
+      expect(response.body).to include "Endereço não pode ficar em branco"
+    end
+
+    it "fail if there an internal error" do
+      allow(Warehouse).to receive(:new).and_raise(ActiveRecord::ActiveRecordError)
+
+      warehouse_params = { 
+        warehouse: {
+          name: 'Belo Horizonte', 
+          code: 'BHZ', 
+          area: 20000, 
+          cep: '64000-000', 
+          city: 'Belo Horizonte', 
+          description: 'Galpão mineiro', 
+          address: 'Av Tiradentes, 20'
+          }
+        }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status 500
+    end
   end
 end
